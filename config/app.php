3845a985fabea233ae626a59dcc5eb03134fbf78
Evgeny <just-moi@mail.ru>
2019-07-26 12:56:31 +0300

    first commit

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        //плагин для phpStorm
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
