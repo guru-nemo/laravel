0ad3fe933b13a45e3806eb60ad2ea7773a7efdab
Evgeny <just-moi@mail.ru>
2019-07-26 13:08:39 +0300

    erwer

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ Route::post('turist/request/add/{user}', 'RequestController@edit');
 Route::get('/turist/request/edit/{request}', 'RequestController@edit');
 Route::post('/turist/request/edit/{request}', 'RequestController@edit');
 Route::delete('/turist/request/delete/{request}', 'RequestController@destroy');
-/////
+
 Auth::routes();
